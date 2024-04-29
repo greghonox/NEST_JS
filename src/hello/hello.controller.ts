b2799857c941efab484314c4b737f4f575cf2fbe
Gregorio honorato <167568976+greghonoazos@users.noreply.github.com>
2024-04-29 15:44:48 -0300

    feat: adding test client

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -5,8 +5,8 @@ import {HelloService} from './hello.service'
 export class HelloController {
     constructor(private readonly hello_service: HelloService) {};
 
-    @Get()
-    async getHello() {
-        return this.hello_service.hello();
+    @Get('comments:comment')
+    async getHello(comment: number = 0) {
+        return this.hello_service.hello(comment);
     }
 }
--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -4,8 +4,8 @@ import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class HelloService {
-    async hello() {
-        const url = 'https://jsonplaceholder.typicode.com/comments'
+    async hello(comment: number=0): Promise<string> {
+        const url = 'https://jsonplaceholder.typicode.com/comments' + (comment == 0 ? '':`/${comment}`);
         const response: AxiosResponse = await axios.get(url);
         return response.data;
     }
